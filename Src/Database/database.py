88304ffb81a5b19b3afb7479a30f759d0d2477a2
Lasse <lass5588@icloud.com>
2025-02-05 13:26:26 +0100

    Script to set up postgres database

--- Src/Database/database.py
+++
@@ -1,6 +0,0 @@
-
-def setup_postgress():
-    pass
-
-if __name__ == "__main__":
-    setup_postgress()
---
+++ Src/Database/setup_database.py
@@ -0,0 +1,98 @@
+import os
+import pyreadstat 
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Database credentials
+USER = "postgres"
+PASSWORD = "postgres"
+HOST = "localhost"
+PORT = "5432"
+DB_NAME = "trial_metadata"
+
+def init_db(data_directory: str):
+    """
+    Inits the database, based on the raw SAS DB, recursively adds all sas7bdat files as tables.
+    Provide the folder path containing all of the SAS files;
+
+    Parameters:
+    - data_directory (str): Root directory to search for SAS files.
+    """
+    sas_files = _find_sas_files(data_directory)
+
+    for _, files in sas_files.items():
+        for sas_file in files:
+            _read_sas(sas_file)
+
+
+def _find_sas_files(directory_path: str):
+    sas_files = {}
+
+    _create_db()
+
+    for root, _, files in os.walk(directory_path):
+        folder_name = os.path.basename(root)
+        sas_paths = [os.path.join(root, f) for f in files if f.endswith('.sas7bdat')]
+
+        if sas_paths:
+            sas_files[folder_name] = sas_paths
+
+    return sas_files
+
+
+def _read_sas(path_to_sas: str):
+    table_name = os.path.splitext(os.path.basename(path_to_sas))[0]
+
+    # meta is all the 'non' visible data, so in our case labels.
+    df, meta = pyreadstat.read_sas7bdat(path_to_sas)
+
+    column_names = df.columns
+
+    label_df = pd.DataFrame({
+        'Table_Name': table_name,
+        'Column_Name': column_names,
+        'Column_Label': meta.column_labels
+    })
+
+    engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}')    
+    
+    try:
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
+        label_df.to_sql('column_label_lookup', engine, if_exists='append', index=False)
+        print(f'Table {table_name} inserted successfully.')
+    except Exception as e:
+        print("Error: ", e)
+
+
+def _create_db():
+    try:
+        conn = psycopg2.connect(dbname="postgres", user=USER, password=PASSWORD, host=HOST, port=PORT)
+        conn.autocommit = True  # Enable auto-commit to execute CREATE DATABASE outside of a transaction
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}'")
+        exists = cur.fetchone()
+
+        if not exists:
+            cur.execute(f"CREATE DATABASE {DB_NAME}")
+            print(f"Database '{DB_NAME}' created successfully.")
+        else:
+            print(f"Database '{DB_NAME}' already exists.")
+
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print("Error creating database:", e)
+
+
+if __name__ == '__main__':
+    data_directory = input("Provide path to the af data folder: ").strip()
+
+    if os.path.isdir(data_directory):
+        try:
+            init_db(data_directory)
+        except Exception as e:
+            print("Failed: ", e)
+    else:
+        print("provided path is not a directory.")

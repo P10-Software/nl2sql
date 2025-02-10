c08e696fb597baff78f0864f898afa9b637f2140
VictorDore <vdha20@student.aau.dk>
2025-02-10 14:30:05 +0100

    Now also measure naturalness of spider 2 columns and tables

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-import os
+from os.path import join, isfile
+from os import listdir, getenv
 import psycopg2
 from libs.SNAILS.snails_naturalness_classifier import CanineIdentifierClassifier
 from dotenv import load_dotenv
-import json
+from json import load, dump
 
 # Database credentials
 load_dotenv()
 
-USER = os.getenv('PG_USER')
-PASSWORD = os.getenv('PG_PASSWORD')
-HOST = os.getenv('PG_HOST')
-PORT = os.getenv('PG_PORT')
-DB_NAME = os.getenv('DB_NAME')
+USER = getenv('PG_USER')
+PASSWORD = getenv('PG_PASSWORD')
+HOST = getenv('PG_HOST')
+PORT = getenv('PG_PORT')
+DB_NAME = getenv('DB_NAME')
 
-def evaluate_naturalness(indentifiers):
-    classifier = CanineIdentifierClassifier()
+SPIDER_DIR_PATH = "Src/Spider2Lite/resource/databases/sqlite"
 
-    classifications = dict()
-    for identifier in indentifiers:
-        classifications[identifier] = classifier.classify_identifier(identifier)[0]["label"]
-
-    distribution_of_classifications = {"distribution": {
-        "N1": len([value for value in classifications.values() if value == "N1"]),
-        "N2": len([value for value in classifications.values() if value == "N2"]),
-        "N3": len([value for value in classifications.values() if value == "N3"])
-    }}
-    
-    return distribution_of_classifications | classifications
-
-def main():
+def evaluate_novo_database_naturalness():
     conn = psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
-    conn.autocommit = True  # Enable auto-commit to execute CREATE DATABASE outside of a transaction
+    conn.autocommit = False  # Enable auto-commit to execute CREATE DATABASE outside of a transaction
     cur = conn.cursor()
 
     #get table names
@@ -51,13 +39,59 @@ def main():
     naturalness_column_label_results = evaluate_naturalness(column_labels)
 
     with open("table_naturalness_results.json", "w") as file:
-        json.dump(naturalness_table_results, file, indent=4)
+        dump(naturalness_table_results, file, indent=4)
 
     with open("column_naturalness_results.json", "w") as file:
-        json.dump(naturalness_column_results, file, indent=4)
+        dump(naturalness_column_results, file, indent=4)
 
     with open("column_label_naturalness_results.json", "w") as file:
-        json.dump(naturalness_column_label_results, file, indent=4)
+        dump(naturalness_column_label_results, file, indent=4)
+
+    cur.close()
+
+def evaluate_spider_2_naturalness():
+    db_paths = [join(SPIDER_DIR_PATH, db_path) for db_path in listdir(SPIDER_DIR_PATH)]
+    column_identifiers = set()
+    table_identifiers = set()
+
+
+    for db_path in db_paths:
+        table_paths = [join(db_path, file) for file in listdir(db_path) if isfile(join(db_path, file)) and file.endswith(".json")]
+
+        # Get all columns (including duplicates)
+        for table_path in table_paths:
+            with open(table_path, "r") as file:
+                table_dict = load(file)
+            column_identifiers.update(table_dict["column_names"])
+            table_identifiers.add(table_dict["table_name"])
+
+    table_naturalness_results = evaluate_naturalness(table_identifiers)
+    column_naturalness_results = evaluate_naturalness(column_identifiers)
+
+    with open("spider2_table_naturalness_results.json", "w") as file:
+        dump(table_naturalness_results, file, indent=4)
+    
+    with open("spider2_column_naturalness_results.json", "w") as file:
+        dump(column_naturalness_results, file, indent=4)
+
+def evaluate_naturalness(indentifiers):
+    classifier = CanineIdentifierClassifier()
+
+    classifications = dict()
+    for identifier in indentifiers:
+        classifications[identifier] = classifier.classify_identifier(identifier)[0]["label"]
+
+    distribution_of_classifications = {"distribution": {
+        "N1": len([value for value in classifications.values() if value == "N1"]),
+        "N2": len([value for value in classifications.values() if value == "N2"]),
+        "N3": len([value for value in classifications.values() if value == "N3"])
+    }}
+    
+    return distribution_of_classifications | classifications
+
+def main():
+    evaluate_novo_database_naturalness()
+    evaluate_spider_2_naturalness()
 
 if __name__ == "__main__":
     main()

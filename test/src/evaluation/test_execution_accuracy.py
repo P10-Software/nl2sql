15cb8beb741be809f8b339a7734a3698451740b6
Lasse <lass5588@icloud.com>
2025-02-14 09:32:47 +0100

    Updated empty test

--- test/src/evaluation/test_execution_accuracy.py
+++ test/src/evaluation/test_execution_accuracy.py
@@ -1,6 +1,5 @@
-import sqlite3
-import pytest
 from unittest.mock import patch
+import pytest
 from src.evaluation.execution_accuracy import execution_accuracy
 
 
@@ -16,7 +15,7 @@ class TestExecutionAccuracy:
         generated_gurey = "SELECT * FROM tab_pln"
 
         assert execution_accuracy(goal_query, generated_gurey)
-    
+
 
     @patch("src.evaluation.execution_accuracy.execute_query")
     def test_execution_accuracy_different(self, mock_execute_query):
@@ -43,15 +42,14 @@ class TestExecutionAccuracy:
         assert not execution_accuracy(goal_query, generated_gurey)
 
 
-    # TODO I do not remember how this is handleds?
     @patch("src.evaluation.execution_accuracy.execute_query")
-    def test_execution_accuracy_match_empty(self, mock_execute_query):
+    def test_execution_accuracy_empty_generated (self, mock_execute_query):
         mock_execute_query.side_effect = [
-            [],
+            [("id1", "Joel"), ("id2", "SmallJoel")],
             []
         ]
 
-        goal_query = "SELECT * FROM non_existing"
+        goal_query = "SELECT DISTINCT * FROM tab_pln"
         generated_gurey = "SELECT DISTINCT * FROM unknown"
 
-        assert execution_accuracy(goal_query, generated_gurey)
+        assert not execution_accuracy(goal_query, generated_gurey)

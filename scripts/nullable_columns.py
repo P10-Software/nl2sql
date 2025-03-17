0d8ef45088e837d138664a0451d6bc62b1d02c54
Lasse <lass5588@icloud.com>
2025-03-17 16:20:11 +0100

    updated nullable columns to only contain the nullable columns

--- scripts/nullable_columns.py
+++ scripts/nullable_columns.py
@@ -11,8 +11,6 @@ DB_PATH_NATURAL = os.getenv('DB_PATH_NATURAL')
 DB_NATURAL = int(os.getenv('DB_NATURAL', 0))
 
 
-#TODO Remember to add one for both abbreviated and natural (Just add to the same)
-
 def find_nullable_columns(db_path, nullable_columns):
     """
     Identify columns that are nullable if they contain any rows containing nulle values.
@@ -37,17 +35,18 @@ def find_nullable_columns(db_path, nullable_columns):
             query = f"SELECT EXISTS(SELECT 1 FROM {table_name} WHERE {column_name} IS NULL LIMIT 1);"
             cursor.execute(query)
             nullable = cursor.fetchall()[0][0]
-            full_column_name = f"{table_name}.{column_name}"
-            nullable_columns.append({"table.column": full_column_name, "nullable": nullable})
+
+            if nullable:
+                nullable_columns.append(f"{table_name}.{column_name}")
 
     conn.close()
-
     return nullable_columns
 
 
 def save_nullable_columns(nullable_columns):
     """
-    Save the nullable_column in a CSV format.
+    Save all nullable columns in in a CSV format.
+    It contains columns for both normalized and abbreviated database.
     """
     df = pd.DataFrame(nullable_columns)
 

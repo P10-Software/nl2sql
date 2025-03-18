4b4df69162324c3c4a1dad23259bc4b20a4e306c
Lasse <lass5588@icloud.com>
2025-03-18 13:23:16 +0100

    Simplyfied generate_mschema

--- scripts/generate_mschema.py
+++ scripts/generate_mschema.py
@@ -11,34 +11,23 @@ DB_PATH_NATURAL = os.getenv('DB_PATH_NATURAL')
 DB_NATURAL = int(os.getenv('DB_NATURAL', 0))
 
 
-def generate_mschema(natural: bool):
+def generate_mschema():
     """
-    Generate schema to M-schema DDL format with additional information
+    Generate M-schema, containing additional information compared to DDL
     """
-    if natural:
-        db_engine = create_engine(f'sqlite:///{DB_PATH_NATURAL}')
-    else:
-        db_engine = create_engine(f'sqlite:///{DB_PATH_ABBREVIATED}')
 
-    return SchemaEngine(engine=db_engine, db_name=DB_NAME).mschema.to_mschema()
+    db_engine = create_engine(f'sqlite:///{DB_PATH_NATURAL}')
+    with open(".local/mschema_natural.txt", "w") as file:
+        file.write(SchemaEngine(engine=db_engine, db_name=DB_NAME).mschema.to_mschema())
 
+    print("Successfully generated M-Schema for natural database")
 
-def save_mschema_file(natural: bool):
-    """
-    Saves the database mschema as a text file in the .local directory
-    Args:
-        - natural (bool): Natural or abbreviated mschema
-    """
-    if natural:
-        with open(".local/mschema_natural.txt", "w") as file:
-            file.write(generate_mschema(natural=natural))
-        print("Successfully generated M-Schema for natural database")
-    else:
-        with open(".local/mschema_abbreviated.txt", "w") as file:
-            file.write(generate_mschema(natural=natural))
-        print("Successfully generated M-Schema for abbreviated database")
+    db_engine = create_engine(f'sqlite:///{DB_PATH_ABBREVIATED}')
+    with open(".local/mschema_abbreviated.txt", "w") as file:
+        file.write(SchemaEngine(engine=db_engine, db_name=DB_NAME).mschema.to_mschema())
+
+    print("Successfully generated M-Schema for abbreviated database")
 
 
 if __name__ == "__main__":
-    save_mschema_file(natural=True)
-    save_mschema_file(natural=False)
+    generate_mschema()

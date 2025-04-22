b969d7418f697f618a0c837db88737be05421300
VictorDore <vdha20@student.aau.dk>
2025-04-22 11:13:59 +0200

    Now also put column names to lower

--- scripts/create_training_set_from_spider.py
+++ scripts/create_training_set_from_spider.py
@@ -27,8 +27,7 @@ def create_training_set():
             goal_columns = []
             for table in column_table.keys():
                 for column in column_table[table]:
-                    goal_columns.append(f"{table} {column}")
-            goal_columns = [s[0].lower() + s[1:] if s else s for s in goal_columns]
+                    goal_columns.append(f"{table.lower()} {column.lower()}")
 
             valid_train_set.append({"question": question, "schema": schema, "goal answer": goal_columns})
         except:
@@ -42,18 +41,14 @@ def _load_schema_for_all_dbs():
     for database_path in database_paths:
         db_id = basename(database_path).split(".")[0]
         db_engine = create_engine(f'sqlite:///{database_path}')
-        schema_dict[db_id] = {"schema": _lowercase_column_names(SchemaEngine(engine=db_engine, db_name=db_id).mschema.to_mschema()), "db_path": database_path}
+        schema_dict[db_id] = {"schema": _lowercase_column_and_table_names(SchemaEngine(engine=db_engine, db_name=db_id).mschema.to_mschema()), "db_path": database_path}
     return schema_dict
 
-def _lowercase_column_names(schema: str) -> str:
-    def lowercase_column(match):
-        column_name = match.group(1)
-        if column_name[0].isupper():
-            column_name = column_name[0].lower() + column_name[1:]
-        return f"({column_name}:{match.group(2)}"
-
-    # Apply to every column inside (column_name:TYPE, ...)
-    updated_schema = re.sub(r"\(\s*(\w+):(\w+)", lowercase_column, schema)
+def _lowercase_column_and_table_names(schema: str) -> str:
+    # Column name to lower
+    updated_schema = re.sub(r"\(\s*(\w+):(\w+)", lambda m: "(" + m.group(1).lower() + ":" + m.group(2), schema)
+    # Table name to lower
+    updated_schema = re.sub(r"(# Table:\s*)([A-Z])(\w+)", lambda m: m.group(1) + m.group(2).lower() + m.group(3), updated_schema)
     return updated_schema
 
 if __name__ == "__main__":

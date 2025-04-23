fb66985d8fec8a2d2a4abba95c045aeb2b08a4e4
Lasse <lass5588@icloud.com>
2025-04-23 09:31:13 +0200

    splitting of mschema

--- src/core/schema_chunking.py
+++ src/core/schema_chunking.py
@@ -1,10 +1,10 @@
-import torch
-from src.core.model_implementations import LlamaModel, DeepSeekLlamaModel, DeepSeekQwenModel, XiYanSQLModel
-from src.core.prompt_strategies import Llama3PromptStrategy, DeepSeekPromptStrategy, XiYanSQLPromptStrategy
-from src.core.base_model import NL2SQLModel
+# import torch
+# from src.core.model_implementations import LlamaModel, DeepSeekLlamaModel, DeepSeekQwenModel, XiYanSQLModel
+# from src.core.prompt_strategies import Llama3PromptStrategy, DeepSeekPromptStrategy, XiYanSQLPromptStrategy
+# from src.core.base_model import NL2SQLModel
+#
 
-
-def chunk_mschema(mschema: str, context_size) -> list[str]:
+def chunk_mschema(mschema: str, context_size):
     """
     Transform the M-Schema into a list of M-Schemas that fits within a given context window
 
@@ -13,31 +13,69 @@ def chunk_mschema(mschema: str, context_size) -> list[str]:
     - context_size: The size of a models context window
     """
 
-    test = mschema.split("#")
-    print(test[92])
-    # [print(x) for x in test]
+    max_mschema_size = context_size // 2
 
-    return [mschema]
+    mschema_split = mschema.split("#")
+    mschema_header_text = mschema_split[0]
+
+    print("header size: ", len(mschema_header_text))
+
+    tables = ['#' + table for table in mschema_split[1:]]
+    tables_tokens_size = [len(table) for table in tables]
+
+    # print(tables[1])
+    # print(mschema)
+
+    chunks = []
+    chunk = ""
+    new_chunk_size = 0
+    for i, table_size in enumerate(tables_tokens_size):
+        new_chunk_size = new_chunk_size + table_size
+        if new_chunk_size < max_mschema_size:
+            chunk = chunk + tables[i]
+        else:
+            print(new_chunk_size)
+            chunks.append(chunk)
+            # break
+            chunk = tables[i]
+            new_chunk_size = 0
+
+    chunks.append(chunk)
+
+    return chunks
+
+
+# def test_token(mschema):
+#     dataset = [{"question": "test question", "golden_query": "test gold"}]
+#     prompt_strategy = XiYanSQLPromptStrategy("sqlite")
+#     model = XiYanSQLModel(dataset, prompt_strategy, mschema=True)
+#
+#     #mschema = mschema.split("#")
+#
+#     prompt = model.prompt_strategy.get_prompt(mschema, "This is a question template")
+#
+#     print(prompt)
+#     print("Characters in prompt: ", len(prompt))
+#     tokens = model.tokenizer(prompt, return_tensors="pt", truncation=False)["input_ids"][0]
+#     print("Tokens in prompt: ", len(tokens))
+#     context_length = getattr(model.model.config, "max_position_embeddings", None)
+#     print("Model context length: ", context_length)
+
 
 if __name__ == "__main__":
     mschema = ""
     with open(".local/mschema_trial_metadata_abbreviated.txt", "r") as file:
         mschema = file.read()
 
-    dataset = [{"question": "test question", "golden_query": "test gold"}]
-    prompt_strategy = XiYanSQLPromptStrategy("sqlite")
-    model = XiYanSQLModel(dataset, prompt_strategy, mschema=True)
 
-    #mschema = mschema.split("#")
+    chunks = chunk_mschema(mschema, 10000)
 
-    prompt = model.prompt_strategy.get_prompt(mschema, "This is a question template")
+    print("Mschema length: ", len(mschema))
+    print("Chunked length: ", len(' '.join(chunks)))
+    # with open("regular_mschema.txt", "w") as file:
+    #     file.write(mschema)
+    # with open("chunked_mschema.txt", "w") as file:
+    #     file.write(' '.join(chunks))
 
-    print(prompt)
-    print("Characters in prompt: ", len(prompt))
-    tokens = model.tokenizer(prompt, return_tensors="pt", truncation=False)["input_ids"][0]
-    print("Tokens in prompt: ", len(tokens))
-    context_length = getattr(model.model.config, "max_position_embeddings", None)
-
-    print("Model context length: ", context_length)
-
-    #chunk_mschema(mschema, 1000)
+    print("Chunk sizes")
+    print([len(x) for x in chunks])

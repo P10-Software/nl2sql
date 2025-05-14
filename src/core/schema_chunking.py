c54d78ab6dafb9bf847a88692c08c07e00fa8eda
Lasse <lass5588@icloud.com>
2025-05-14 13:51:40 +0200

    Chunk mschema without relations works with relations now.

--- src/core/schema_chunking.py
+++ src/core/schema_chunking.py
@@ -1,7 +1,9 @@
-import torch
+# import torch
+from sqlalchemy import create_engine
+from mschema.schema_engine import SchemaEngine
 
 
-def chunk_mschema(mschema: str, model) -> list[str]:
+def chunk_mschema_no_relations(mschema: str, model) -> list[str]:
     """
     Transform the M-Schema into a list of M-Schemas that fits within a given context window.
     Each chunk contains only whole tables from the mschema.
@@ -11,9 +13,10 @@ def chunk_mschema(mschema: str, model) -> list[str]:
     - context_size: The size of a models context window
     """
 
-    context_size = getattr(model.model.config, "max_position_embeddings", None)
-    if context_size is None:
-        raise ValueError("Model context size (max_position_embeddings) is not set.")
+    context_size = 10000
+    # context_size = getattr(model.model.config, "max_position_embeddings", None)
+    # if context_size is None:
+    #     raise ValueError("Model context size (max_position_embeddings) is not set.")
 
     max_mschema_size = context_size // 1.5
 
@@ -21,11 +24,15 @@ def chunk_mschema(mschema: str, model) -> list[str]:
     mschema_header_text = mschema_split[0]
     mschema_tables = ['#' + table for table in mschema_split[1:]]
 
+    if "【Foreign keys】" in mschema_tables[-1]:
+        mschema_tables[-1] = mschema_tables[-1].split("【Foreign keys】")[0]
+
     chunks = []
     chunk = ""
     new_chunk_size = 0
     for table in mschema_tables:
-        table_size = len(model.tokenizer(table, return_tensors="pt", truncation=False)["input_ids"][0])
+        # table_size = len(model.tokenizer(table, return_tensors="pt", truncation=False)["input_ids"][0])
+        table_size = len(table)
         new_chunk_size = new_chunk_size + table_size
         if new_chunk_size > max_mschema_size:
             if chunk:
@@ -39,3 +46,23 @@ def chunk_mschema(mschema: str, model) -> list[str]:
         chunks.append(mschema_header_text + chunk)
 
     return chunks
+
+
+def generate_mschema():
+    db_engine = create_engine('sqlite:///.local/train/train_databases/works_cycles/works_cycles.sqlite')
+    with open(".local/mschema_chunking_test.txt", "w") as file:
+        file.write(SchemaEngine(engine=db_engine, db_name="works_cycles").mschema.to_mschema())
+
+
+if __name__ == "__main__":
+    mschema: str
+
+    generate_mschema()
+
+    with open(".local/mschema_chunking_test.txt", "r") as file:
+        mschema = file.read()
+
+    chunks = chunk_mschema_no_relations(mschema, 1)
+    for chunk in chunks:
+        print(chunk)
+    print(len(chunks))

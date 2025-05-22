85a980a6e53df13c2f0ae424959845a9b7275019
VictorDore <vdha20@student.aau.dk>
2025-05-22 21:01:52 +0200

    Changed ordering

--- test/scripts/test_schema_linker_train.py
+++ test/scripts/test_schema_linker_train.py
@@ -11,7 +11,7 @@ with patch.dict('sys.modules', {
         # arrange
         question = "This is some mock question"
         schema = "This is some mock schema"
-        goal_columns = ["users user_id", "orders amount"]
+        goal_columns = ["orders amount", "users user_id"]
         input_data = [{"question": question, "schema": schema, "goal answer": goal_columns}]
         k = 5
         expected_result = [{"question": question, "goal columns": goal_columns, "RMC efficiency": 0.5, "top k columns": ["orders order_id", "users user_id", "users name", "orders amount", "orders user_id"], "top k relevance": [0.8, 0.7, 0.65, 0.65, 0.6], "column recall@k": 1.0, "table recall@k": 1.0}, 
